6d025886865a1a88e09fac940ae1a505da245928
Andreas Krahl <A.Krahl90@gmail.com>
2018-01-21 14:44:10 +0100

    working site for server/ test stuff

---
+++ WebApplication1/HitCounterHub.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class HitCounterHub : Hub
+    {
+        private static int _hitCounter = 0;
+
+        public void RecordHit(string message, string id)
+        {
+            _hitCounter += 1;
+
+            this.Clients.All.InvokeAsync("Hit", message, id);
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            _hitCounter -= 1;
+
+            Clients.All.InvokeAsync("Hit", this.Context.Connection.ConnectionAbortedToken.CanBeCanceled + " has left");
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+}
---
+++ WebApplication1/wwwroot/js/site.js
@@ -0,0 +1,41 @@
+﻿//// Write your Javascript code.
+//(function () {
+
+//    let connection = new signalR.HubConnection("http://localhost:64644/hitCounter")
+
+//    connection.on('Hit', function (color, id) {
+//        console.log(color);
+//        // $('#counter').text(data);
+//        setRandomColor(color, id);
+
+//    });
+
+//    connection.start()
+//        // .then(() => connection.invoke('recordHit', getMessage()));
+//        .then(function () {
+//            $('.box__inner').on('click touchstart', function () {
+//                var that = this;
+//                connection.invoke('recordHit', randomColor(), that.id);
+//            });
+//        });
+
+//    function getMessage() {
+//        var text = $('#input').val();
+
+//        return text;
+//    }
+
+//    function randomColor() {
+//        var letters = '0123456789ABCDEF';
+//        var color = '#';
+//        for (let i = 0; i < 6; i++) {
+//            color += letters[Math.floor(Math.random() * 16)];
+//        }
+
+//        return color;
+//    }
+
+//    function setRandomColor(color, id) {
+//        $('#' + id).css('background-color', color);
+//    }
+//})();
