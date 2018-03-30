07877d750b0d1d3d21e919fd1b4c9cb9394ff537
Andreas Krahl <A.Krahl90@gmail.com>
2018-03-30 20:15:05 +0200

    no message

--- app/TicTacToe.Web/TicTacToe/Game/Controllers/GameController.cs
+++ app/TicTacToe.Web/TicTacToe/Game/Controllers/GameController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +9,7 @@ namespace TicTacToe.Web.TicTacToe.Game.Controllers
 {
     public class GameController : Controller
     {
-
+        [Authorize]
         public IActionResult Index()
         {
             return View("~/TicTacToe/Game/Views/Index.cshtml");
