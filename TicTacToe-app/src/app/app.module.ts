ecfefc0b936c4b52d55bfa80fc49ac8087e9cf3e
Andreas Krahl <andreas.krahl@netzkern.de>
2019-08-11 20:32:18 +0200

    renamed folder; fixed some files

--- TicTacToe-app/src/app/app.module.ts
+++ TicTacToe-app/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ContentLayoutComponent } from './layout/content-layout/content-layout.component';
-import { HeaderComponent } from './layout/nav/header.component';
+import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { AuthLayoutComponent } from './layout/auth-layout/auth-layout.component';
 import { BrowserModule } from '@angular/platform-browser';
