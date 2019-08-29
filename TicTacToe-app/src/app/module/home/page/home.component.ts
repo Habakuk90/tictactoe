0469da938fd6a4a7d7cf993808a0268ac07e28c8
Andreas Krahl <andreas.krahl@netzkern.de>
2019-08-29 22:00:18 +0200

    removed legacy code; this need to be implemented HUBCONNECTION as reference

--- TicTacToe-app/src/app/module/home/page/home.component.ts
+++ TicTacToe-app/src/app/module/home/page/home.component.ts
@@ -25,8 +25,6 @@ export class HomeComponent implements OnInit, OnDestroy, HubComponent {
   constructor(
     private userService: UserService,
     private ghost: GhostService) {
-
-    // this.userService._HomeStateSubject.subscribe(x => this.selectionState = x);
   }
 
   selectedGame(game: IGame): IGame {
@@ -41,135 +39,8 @@ export class HomeComponent implements OnInit, OnDestroy, HubComponent {
   }
 
   ngOnDestroy() {
-    // evaluate if more socketuris are an option.
-    // this.hub.stopConnection();
   }
 
   registerOnMethods() {
-    // this.hub = this.hubService.createConnection('/signalR', 'homehub', HomeHubConnection);
-
-
-    // that.hub.isConnected.subscribe((isConnected: boolean) => {
-    //   if (isConnected) {
-    //     that.hub.addCurrentUser(that.userService.currentUserName, that.userService.isAnonymous);
-    //     that.registerOnMethods();
-    //   }
-    // });
-
-    // this.hub.onUpdateUserList((userOnline: Array<IUser>) => {
-    //   that.userService.userOnline = userOnline;
-    // });
-
-    // this.hub.onStartGame((groupName: string, gameName: string) => {
-    //   that.groupService._groupNameSubject.next(groupName);
-    //   that.router.navigate(['/' + gameName]);
-    // });
-
-    // this.hub.onOpenModal((enemy: string, gameName: string, modalName: string) => {
-    //   if (that.selectedGames) {
-    //     that.selectedGames.find(x => x.name.toLowerCase() === gameName.toLowerCase()).selected = true;
-    //   }
-
-    // const name: Modals = Modals[modalName];
-    // const modal: IModal = new Modal(name, { enemyUserName: enemy });
-    // that.modalService.openModal(modal);
-
-    // this.hub.challengePlayer(this.selectedPlayer.name, 'tictactoe');
-
-    // });
   }
 }
-
-// class GhostDOMParser extends DOMParser {
-//   constructor(private html: string) {
-//     super();
-//   }
-// }
-
-abstract class GhostElement {
-  abstract beginstring: string;
-  abstract endstring: string;
-
-  constructor() {
-
-  }
-}
-
-export interface IGhostElement {
-  html: Document;
-}
-
-// class GhostElementFactory {
-//   private elements: IGhostElement[];
-//   private readonly _html: string;
-
-
-//   constructor(private document: Document) {
-//     this.elements = this.getElements(document);
-//   }
-
-//   createElements(element: IGhostElement) {
-//     this.getElement(this.document.querySelector('body').innerHTML, element);
-
-//     return this.document.querySelectorAll(':not(body):not(html):not(head)');
-//   }
-
-//   getAll<T extends IGhostElement>(): GhostImageElement[] {
-//     const imageFigues: GhostImageElement[] = [];
-
-
-
-//     return imageFigues;
-//   }
-
-//   getImage() {
-
-//   }
-
-//   createByString() {
-
-//   }
-
-//   private getElement(html: string, element: IGhostElement) {
-//     const beginOf = html.indexOf(element.beginstring);
-//     const endOf = html.indexOf(element.endstring);
-//     html = html.substring(beginOf + element.beginstring.length, endOf);
-
-//     const parser = new DOMParser();
-//     element.html = parser.parseFromString(html, 'text/html');
-
-//     console.log(element);
-
-//     return element;
-//   }
-
-//   createElement() {
-
-//   }
-//   private getElements(document: Document): IGhostElement[] {
-//     let first: number = this.document.querySelector('body').innerHTML.indexOf('<!--kg-card-begin: ') + '<!--kg-card-begin: '.length;
-
-//     let element: IGhostElement = new GhostElement();
-
-//     // let elements: I = new DOMParser().parseFromString(this.document.querySelector('body').innerHTML, 'text/html');
-
-//     return elements;
-//   }
-// }
-
-
-// class GhostElement implements IGhostElement {
-//   private beginstring: string;
-//   private endstring: string;
-//   private html: Document;
-
-//   constructor(document: Document) {
-//     this
-//   }
-// }
-
-// class GhostImageElement extends GhostElement {
-//   private beginstring = '<!--kg-card-begin: image-->';
-//   private endstring = '<!--kg-card-end: image-->';
-// }
-// }
