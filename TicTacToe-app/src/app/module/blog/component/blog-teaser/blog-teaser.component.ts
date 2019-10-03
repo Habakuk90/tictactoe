f74f67b8b59b96f124e10769526c490e0722170a
Andreas Krahl <andreas.krahl@netzkern.de>
2019-10-03 10:08:24 +0200

    removed unused stuff

--- TicTacToe-app/src/app/module/blog/component/blog-teaser/blog-teaser.component.ts
+++ TicTacToe-app/src/app/module/blog/component/blog-teaser/blog-teaser.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { IResponse } from 'src/app/shared/http/responseParams';
 
-
 @Component({
   selector: 'app-blog-teaser',
   templateUrl: 'blog-teaser.component.html',
