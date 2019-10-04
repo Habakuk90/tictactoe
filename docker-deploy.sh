997abeb1c9e71a70753fa411349634675361a474
Andreas Krahl <andreas.krahl@netzkern.de>
2019-10-04 15:44:18 +0200

    travis test

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+if [ -n "$GHOST_API_KEY" ]; then
+    sed -i 's/GHOST_API_KEY/GHOST_API_KEY_REPLACE/g' src/environments/environment.prod.ts
+fi
+
 if [ "$TRAVIS_BRANCH" == "master" ]; then
   echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
   docker build -t wahnsinnshub/ttt-repo:latest .
