d817b7d600d6da3698d35e7b80f938455d80161e
Andreas Krahl <andreas.krahl@netzkern.de>
2019-10-04 15:16:13 +0200

    added environment variable

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 if [ "$TRAVIS_BRANCH" == "master" ]; then
+  echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
   docker build -t wahnsinnshub/ttt-repo:latest .
   docker push wahnsinnshub/ttt-repo:latest
 fi
