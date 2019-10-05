10a984a40a8fb3be0e8fe2718c7b50d768e86562
Andreas Krahl <andreas.krahl@netzkern.de>
2019-10-05 21:52:17 +0200

    ja nein vielleicht?

--- TicTacToe-app/extra-webpack.config.js
+++ TicTacToe-app/extra-webpack.config.js
@@ -4,9 +4,7 @@ module.exports = {
   plugins: [
     // new webpack.EnvironmentPlugin(['KEY', 'SQLSERVER']),
     new webpack.DefinePlugin({
-      'process.env': {
-        GHOST_API_KEY: process.env.GHOST_API_KEY,
-      }
+      'process.env.GHOST_API_KEY': JSON.stringify(process.env.GHOST_API_KEY)
     })
   ]
 }
