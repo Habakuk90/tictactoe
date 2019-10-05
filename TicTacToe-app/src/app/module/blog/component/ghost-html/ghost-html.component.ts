fa97d133a8120a35c96fa96e489a3d09e2b47e2a
Andreas Krahl <andreas.krahl@netzkern.de>
2019-10-05 13:13:07 +0200

    styling

--- TicTacToe-app/src/app/module/blog/component/ghost-html/ghost-html.component.ts
+++ TicTacToe-app/src/app/module/blog/component/ghost-html/ghost-html.component.ts
@@ -1,22 +1,33 @@
-import { Component, ElementRef, Input } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, DoCheck } from '@angular/core';
 import { IResponse } from 'src/app/shared/http/responseParams';
+import { parse } from 'querystring';
 
 
 @Component({
   selector: 'app-ghost-html',
   template: `
-    <div *ngIf="post?.feature_image" class="ghost-html__image l-outbreak">
+  <div *ngIf="post?.title" class="ghost-html__title">
+    <h1>{{post?.title}}</h1>
+  </div>
+  <div *ngIf="post?.feature_image" class="ghost-html__image l-outbreak">
         <img class="a-image" [src]="post?.feature_image" alt="">
     </div>
-    <div *ngIf="post?.title" class="ghost-html__title">
-      <h2>{{post?.title}}</h2>
-    </div>
-    <div [innerHTML]="post.html" class="ghost-html"></div>
+    <div [innerHTML]="post?.html" class="ghost-html"></div>
   `,
   styleUrls: ['ghost-html.component.scss']
 })
-export class GhostHtmlComponent {
+export class GhostHtmlComponent implements DoCheck {
   @Input() post: IResponse = null;
   constructor() {
+  }
+
+  ngDoCheck() {
+    if (this.post != null && this.post.html) {
+      const parser = new DOMParser();
+      let array: NodeListOf<ChildNode>;
+      const document = parser.parseFromString(this.post.html, 'text/html');
+      array = document.querySelector('body').childNodes;
+      array.forEach(x => console.log(x));
+    }
   }
 }
