d5366c5cbd9756d0660a2156f04e1e6750b06649
Andreas Krahl <andreas.krahl@netzkern.de>
2019-10-05 22:09:01 +0200

    uff

--- TicTacToe-app/extra-webpack.config.js
+++ TicTacToe-app/extra-webpack.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack');
 
 module.exports = {
   plugins: [
-    new webpack.EnvironmentPlugin(['GHOST_API_KEY'])
+    new webpack.EnvironmentPlugin(['GHOST_API_KEY']),
     new webpack.DefinePlugin({
       'process.env.GHOST_API_KEY': process.env.GHOST_API_KEY
     })
