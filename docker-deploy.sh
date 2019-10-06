d30f619931328358b118f20d016ac271a6af935f
Andreas Krahl <andreas.krahl@netzkern.de>
2019-10-06 11:09:42 +0200

    i dont think so

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # if [ "$TRAVIS_BRANCH" == "master" ]; then
   echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
   docker build -t wahnsinnshub/ttt-repo:CITEST .
