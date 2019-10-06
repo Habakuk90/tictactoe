c453c7d8d321dfc503a579aa3668b472a02c812f
Andreas Krahl <andreas.krahl@netzkern.de>
2019-10-06 21:00:11 +0200

    finally?

--- TicTacToe-app/extra-webpack.config.js
+++ TicTacToe-app/extra-webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     // new webpack.EnvironmentPlugin(['GHOST_API_KEY']),
     new webpack.DefinePlugin({
       'process.env': {
-        GHOST_API_KEY: JSON.stringify(process.env.GHOST_KEY),
+        GHOST_API_KEY: JSON.stringify(process.env.GHOST_API_KEY),
         NODE_ENV: JSON.stringify(process.env.NODE_ENV)
       },
     })
--- docker-deploy.sh
+++ docker-deploy.sh
@@ -2,6 +2,6 @@
 
 # if [ "$TRAVIS_BRANCH" == "master" ]; then
   echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-  docker build -t wahnsinnshub/ttt-repo:CITEST .
+  docker build --build-arg key=${GHOST_API_KEY} -t wahnsinnshub/ttt-repo:CITEST .
   docker push wahnsinnshub/ttt-repo:CITEST
 # fi
