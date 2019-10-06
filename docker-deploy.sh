86821c30e06a18bafe4e9d6dde59b9761226d2a6
Andreas Krahl <andreas.krahl@netzkern.de>
2019-10-06 21:10:48 +0200

    guess thats it

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# if [ "$TRAVIS_BRANCH" == "master" ]; then
+if [ "$TRAVIS_BRANCH" == "master" ]; then
   echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-  docker build --build-arg key=${GHOST_API_KEY} -t wahnsinnshub/ttt-repo:CITEST .
-  docker push wahnsinnshub/ttt-repo:CITEST
-# fi
+  docker build --build-arg key=${GHOST_API_KEY} -t wahnsinnshub/ttt-repo:latest .
+  docker push wahnsinnshub/ttt-repo:latest
+fi
