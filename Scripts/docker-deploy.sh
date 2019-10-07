32652576cf8b50b838703e3e5f0212950b57c843
Andreas Krahl <andreas.krahl@netzkern.de>
2019-10-07 21:45:37 +0200

    well no.. gotta configure the conditional via travis.yml in best case

--- Scripts/docker-deploy.sh
+++ Scripts/docker-deploy.sh
@@ -4,11 +4,10 @@
 #     sed -i 's/GHOST_API_KEY/('"$GHOST_API_REPLACE"')/g' src/environments/environment.prod.ts
 # fi
 
-# If we are building develop we are pushing the latest tag
-if [ "$TRAVIS_BRANCH" == "develop" ]; then
+if [ "$TRAVIS_BRANCH" == "master" ]; then
   echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
   docker build --build-arg key=${GHOST_API_KEY} -t wahnsinnshub/ttt-repo:latest .
   docker push wahnsinnshub/ttt-repo:latest
 fi
 
-# TODO If we are buildin master we want to push by the latest release number
+# TODO If we are build as release we want to push by the latest release number
