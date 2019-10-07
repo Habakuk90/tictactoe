456c45dd21353090d3b5c0d7a35890650eca0dee
Andreas Krahl <andreas.krahl@netzkern.de>
2019-10-07 21:24:44 +0200

    push latest tag on develop

--- Scripts/docker-deploy.sh
+++ Scripts/docker-deploy.sh
@@ -4,8 +4,11 @@
 #     sed -i 's/GHOST_API_KEY/('"$GHOST_API_REPLACE"')/g' src/environments/environment.prod.ts
 # fi
 
-if [ "$TRAVIS_BRANCH" == "master" ]; then
+# If we are building develop we are pushing the latest tag
+if [ "$TRAVIS_BRANCH" == "develop" ]; then
   echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
   docker build --build-arg key=${GHOST_API_KEY} -t wahnsinnshub/ttt-repo:latest .
   docker push wahnsinnshub/ttt-repo:latest
 fi
+
+# TODO If we are buildin master we want to push by the latest release number
