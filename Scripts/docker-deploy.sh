ee4fec1234c84680f28b2e6c978940f60c9a7091
Andreas Krahl <andreas.krahl@netzkern.de>
2019-10-07 12:20:30 +0200

    move bash scripts to scripts fodler

--- Scripts/docker-deploy.sh
+++ Scripts/docker-deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ -z $GHOST_API_REPLACE ]; then
-    sed -i 's/GHOST_API_KEY/('"$GHOST_API_REPLACE"')/g' src/environments/environment.prod.ts
-fi
+# if [ -z $GHOST_API_REPLACE ]; then
+#     sed -i 's/GHOST_API_KEY/('"$GHOST_API_REPLACE"')/g' src/environments/environment.prod.ts
+# fi
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
   echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
